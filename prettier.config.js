24bb1c1451bd1f1f4a7168fea092edd82442bf13
Woojin Lee <nijoow1127@gmail.com>
2024-05-18 12:30:41 +0900

    🎉moharu 프로젝트 시작

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  useTabs: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
