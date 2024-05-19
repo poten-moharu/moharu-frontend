c677b416eff28383711801dd1cdfe6db1d60b162
JinjuNa <dindoo0524@gmail.com>
2024-05-19 10:35:02 +0900

    * bottom navigation bar 에 있는 위시리스트, 마이페이지 화면 생성

---
+++ src/app/my-page/page.tsx
@@ -0,0 +1,7 @@
+export default function MyPage() {
+  return (
+    <main className="flex h-full w-full items-center justify-center">
+      MY PAGE
+    </main>
+  );
+}
---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,7 @@
+export default function Wishlist() {
+  return (
+    <main className="flex h-full w-full items-center justify-center">
+      WISHLIST
+    </main>
+  );
+}
