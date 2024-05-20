82456028347def22055bd260c955cbb0131b2ec5
JinjuNa <113400384+NaJinju@users.noreply.github.com>
2024-05-21 08:54:13 +0900

    *** category bar cursor-pointer 처리

--- src/app/_components/category-bar/category-bar.tsx
+++ src/app/_components/category-bar/category-bar.tsx
@@ -9,7 +9,7 @@ const CategoryBar: React.FC<CategoryBarProps> = ({ categoryList }) => {
   return (
     <div className="scroll-x flex justify-between p-5">
       {categoryList.map(category => (
-        <div key={category.categoryId}>
+        <div key={category.categoryId} className="cursor-pointer">
           <Image
             src={`/images/icons/${category.icon}.svg`}
             alt={category.name}
