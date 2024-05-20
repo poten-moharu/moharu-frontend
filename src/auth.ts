6ebd7f1f47531e442a7d84b3c906cf5f66c8b33e
Woojin Lee <nijoow1127@gmail.com>
2024-05-21 00:02:45 +0900

    *next-auth 설치 및 세팅

---
+++ src/auth.ts
@@ -0,0 +1,7 @@
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import Kakao from 'next-auth/providers/kakao';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google, Kakao],
+});
