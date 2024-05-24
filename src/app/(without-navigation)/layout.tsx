ae7b7e3e03460e26e48be059a8266b951432afa4
Woojin Lee <nijoow1127@gmail.com>
2024-05-24 21:05:49 +0900

    ***레이아웃 css 수정

--- src/app/(without-navigation)/layout.tsx
+++ src/app/(without-navigation)/layout.tsx
@@ -1,5 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="flex flex-auto flex-col overflow-auto">{children}</main>
-  );
+  return <main className="flex flex-auto flex-col">{children}</main>;
 }
