94d8070569a018a02b413e08faf3ebd067e7a38f
Woojin Lee <nijoow1127@gmail.com>
2024-05-26 02:28:42 +0900

    *** 하단 버튼 높이조절

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
-        big: 'w-full h-12 rounded-none',
+        big: 'w-full h-12 rounded-none flex-none',
         icon: 'h-9 w-9',
       },
     },
