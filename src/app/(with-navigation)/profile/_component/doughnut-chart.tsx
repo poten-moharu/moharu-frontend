52d5da9aeaca19e710107b839eef8af511baae3e
나개발 <106743020+dindoo0524@users.noreply.github.com>
2024-05-25 22:49:27 +0900

    feat: #MO-59 [FE] 좋아요 post 기능 및 프로필 화면 작업 (#32)
    * 좋아요 POST 기능
    * session 관련 임시 주석
    * 프로필 화면 처리

--- src/app/(with-navigation)/profile/_component/doughnut-chart.tsx
+++ src/app/(with-navigation)/profile/_component/doughnut-chart.tsx
@@ -26,8 +26,24 @@ const DoughnutChart = ({ data }: { data: [number, number, number] }) => {
   };
 
   return (
-    <div className="relative h-60 w-60 p-7">
-      <Doughnut options={options} data={chartData} />
+    <div className="flex">
+      <div className="relative h-60 w-60 p-7">
+        <Doughnut options={options} data={chartData} />
+      </div>
+      <div className="flex h-[180px] flex-col justify-center">
+        <div className="flex h-32px items-center">
+          <div className="mr-12px h-12px w-12px rounded-full bg-slate-900"></div>
+          예술
+        </div>
+        <div className="flex h-32px items-center">
+          <div className="mr-12px h-12px w-12px rounded-full bg-pink-500"></div>
+          책
+        </div>
+        <div className="flex h-32px items-center">
+          <div className="mr-12px h-12px w-12px rounded-full bg-indigo-300"></div>
+          커리어
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/app/_components/activity/wish-button.tsx
@@ -0,0 +1,61 @@
+import { toast } from '@/components/ui/use-toast';
+import { fetchWithToken } from '@/lib/fetch';
+import { HeartIcon } from 'lucide-react';
+import { useState } from 'react';
+import { LikeToast } from './like-toast';
+import HeartFillIcon from '/public/images/icons/heart-fill.svg';
+
+interface WishButtonProps {
+  isWish: boolean;
+  activityId: number;
+  className?: string;
+}
+
+const WishButton: React.FC<WishButtonProps> = ({
+  isWish: initialIsWish,
+  activityId,
+  className,
+}) => {
+  const [isWish, setIsWish] = useState(initialIsWish);
+
+  const handleClick = async () => {
+    const response = await fetchWithToken(
+      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/activity-wishes`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          activitiesId: activityId,
+        }),
+      },
+    );
+    console.log(response, response.status);
+
+    if (response) {
+      setIsWish(!isWish);
+      LikeToast({ isWish: isWish ?? false, id: activityId });
+    } else {
+      toast({
+        description: '위시리스트 추가에 실패했습니다.',
+      });
+    }
+  };
+
+  return (
+    <button className={className} onClick={handleClick}>
+      {isWish ? (
+        <HeartFillIcon
+          width={24}
+          height={24}
+          className="fill-pink-500 stroke-pink-500"
+        />
+      ) : (
+        <HeartIcon width={24} height={24} className="stroke-slate-900" />
+      )}
+    </button>
+  );
+};
+
+export default WishButton;
