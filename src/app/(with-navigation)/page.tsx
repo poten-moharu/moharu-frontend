bd3db7bc31d0c9e2a2105b5df18e13adde3a07aa
JinjuNa <113400384+NaJinju@users.noreply.github.com>
2024-05-26 11:43:29 +0900

    * 메인화면 수정사항 (첫번째 탭, 상단 fixed)

--- src/app/(with-navigation)/page.tsx
+++ src/app/(with-navigation)/page.tsx
@@ -50,19 +50,29 @@ export default function Home() {
   const handleSelect = (date?: Date) => {
     setSelectedDate(date);
     setOpen(false);
-    // TODO: 달력 선택 시, 달력 닫히도록 처리
-    // onClose();
   };
 
   const fetchCategories = () => {
     fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/activities-category`)
       .then(res => res.json())
       .then(data => {
+        let firstCateogry;
+        if (session) {
+          firstCateogry = {
+            name: '추천',
+            icon: 'package-3',
+          };
+        } else {
+          firstCateogry = {
+            name: '전체',
+            icon: 'list',
+          };
+        }
         data.unshift({
           id: '',
-          name: '전체',
+          name: firstCateogry.name,
           description: '전체 내용',
-          icon: 'list',
+          icon: firstCateogry.icon,
         });
         setCategoryList(data);
       })
@@ -101,56 +111,60 @@ export default function Home() {
 
   return (
     <>
-      {/* TODO: 날짜 포맷을 위한 moment 사용 여부 */}
-      <div className="px-24px pb-12px pt-24px">
-        <Popover open={open}>
-          <PopoverTrigger asChild>
-            <div
-              className="flex w-full cursor-pointer rounded-full border-[1px] border-[#E2E8F0] px-[20px] py-[18px]"
-              onClick={() => setOpen(true)}
-            >
-              <Image
-                src="/images/icons/calendar-days.svg"
-                alt="캘린더"
-                width={24}
-                height={24}
-                className="mr-[14px]"
-              />
-              {/* <CalendarDaysIcon className="mr-[14px] h-6 w-6" /> */}
+      <div className="fixed z-10 w-[448px] bg-white">
+        <div className="px-24px pb-12px pt-24px">
+          <Popover open={open}>
+            <PopoverTrigger asChild>
+              <div
+                className="flex w-full cursor-pointer rounded-full border-[1px] border-[#E2E8F0] px-[20px] py-[18px]"
+                onClick={() => setOpen(true)}
+              >
+                <Image
+                  src="/images/icons/calendar-days.svg"
+                  alt="캘린더"
+                  width={24}
+                  height={24}
+                  className="mr-[14px]"
+                />
+                {/* <CalendarDaysIcon className="mr-[14px] h-6 w-6" /> */}
 
-              <span>
-                {selectedDate ? (
-                  format(selectedDate, 'MMMM do (E)', { locale: ko })
-                ) : (
-                  <span>날짜를 선택 해 주세요</span>
-                )}
-              </span>
-            </div>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0">
-            <Calendar
-              mode="single"
-              selected={selectedDate}
-              onSelect={handleSelect}
-              initialFocus
-              locale={ko}
-            />
-          </PopoverContent>
-        </Popover>
+                <span>
+                  {selectedDate ? (
+                    format(selectedDate, 'MMMM do (E)', { locale: ko })
+                  ) : (
+                    <span>날짜를 선택 해 주세요</span>
+                  )}
+                </span>
+              </div>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Calendar
+                mode="single"
+                selected={selectedDate}
+                onSelect={handleSelect}
+                initialFocus
+                locale={ko}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+        {/* TODO: 카테고리 바 sticky 처리 여부 */}
+        <CategoryBar
+          categoryList={categoryList}
+          selectedCategoryId={selectedCategoryId}
+          onCategorySelect={setSelectedCategoryId}
+        />
       </div>
-      {/* TODO: 카테고리 바 sticky 처리 여부 */}
-      <CategoryBar
-        categoryList={categoryList}
-        selectedCategoryId={selectedCategoryId}
-        onCategorySelect={setSelectedCategoryId}
-      />
-      {activities.length > 0
-        ? activities?.map(activity => (
-            <MainActivityCard key={activity.id} activity={activity} />
-          ))
-        : null}
-      {/* TODO: footer 컴포넌트 분리 및 UI, link 작업 */}
-      <Footer />
+
+      <div className="mt-[180px]">
+        {activities.length > 0
+          ? activities?.map(activity => (
+              <MainActivityCard key={activity.id} activity={activity} />
+            ))
+          : null}
+        {/* TODO: footer 컴포넌트 분리 및 UI, link 작업 */}
+        <Footer />
+      </div>
     </>
   );
 }
